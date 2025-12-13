24b08f6835bdbb2c379cd93eab40e005cd128b28
Afinarf <afinafin024@gmail.com>
2025-12-13 20:46:39 +0700

    hero update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+'use client'
 import About from "../components/about";
 import Hero from "../components/hero";
 import Navbar from "../components/navbar";
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -4,12 +4,12 @@ import Img from "next/image"
 
 export default function Hero () {
     return (
-        <section className="h-auto bg-white">
-            <div className="flex flex-col lg:flex-row justify-between items-start lg:items-end pt-10 pb-10 lg:pb-20 px-4 lg:px-10">
+        <section className="w-full bg-white">
+            <div className="flex flex-col lg:flex-row justify-between pt-10 pb-20 px-10">
 
                 {/* HEADER */}
-                <div className="w-full lg:w-8/12 mb-8 lg:mb-0">
-                    <h1 className="text-black text-4xl md:text-6xl lg:text-7xl">
+                <div className="w-8/12 mb-8">
+                    <h1 className="text-black text-7xl">
                         Your Reliable Partner <br />
                         for Meeting & Event <br />
                         Solutions.
@@ -17,29 +17,29 @@ export default function Hero () {
                 </div>
 
                 {/* BUTTON */}
-                <div className="flex flex-col w-full lg:w-4/12 gap-4 items-start lg:items-end">
-                    <div className="flex flex-wrap justify-start lg:justify-end gap-2 md:gap-4">
-                    <ButtonOutline href="/projector" className="text-base md:text-xl">
+                <div className="flex flex-col w-4/12 gap-4 justify-end">
+                    <div className="flex flex-wrap justify-end gap-2">
+                    <ButtonOutline href="/projector" className="text-base">
                         Projector
                     </ButtonOutline>
-                    <ButtonOutline href="/screen" className="text-base md:text-xl">
+                    <ButtonOutline href="/screen" className="text-base">
                         Screen
                     </ButtonOutline>
-                    <ButtonOutline href="/tv" className="text-base md:text-xl">
+                    <ButtonOutline href="/tv" className="text-base">
                         TV
                     </ButtonOutline>
-                    <ButtonOutline href="/laptop" className="text-base md:text-xl">
+                    <ButtonOutline href="/laptop" className="text-base">
                         Laptop
                     </ButtonOutline>
                     </div>
-                    <div className="flex flex-wrap justify-start lg:justify-end gap-2 md:gap-4">
-                    <ButtonOutline href="/cabel" className="text-base md:text-xl">
+                    <div className="flex flex-wrap justify-end gap-2">
+                    <ButtonOutline href="/cabel" className="text-base">
                         Cabel
                     </ButtonOutline>
-                    <ButtonOutline href="/mic-delegates" className="text-base md:text-xl">
+                    <ButtonOutline href="/mic-delegates" className="text-base">
                         Mic Delegates
                     </ButtonOutline>
-                    <ButtonOutline href="/interpreter" className="text-base md:text-xl">
+                    <ButtonOutline href="/interpreter" className="text-base">
                         Interpreter
                     </ButtonOutline>
                     </div>
@@ -48,7 +48,7 @@ export default function Hero () {
 
             {/* IMAGE */}
             <div className="shrink-0">
-                <div className="w-full h-[300px] md:h-[400px] lg:h-[513px] relative overflow-hidden">
+                <div className="w-full h-[300px] relative overflow-hidden">
                     <Img 
                         src="/img/hero.png" 
                         alt="Hero Image" 
